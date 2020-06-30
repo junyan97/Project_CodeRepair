d55f7c3a54e298c06b82f04f755e4b74e31dc26c
junyan97 <junyan_97@live.com>
2020-06-30 19:32:30 +0800

    added mutation utility

---
+++ mutationUtility.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class mutUtil {
+    public:
+
+        mutUtil(string path) {
+
+        }
+    private:
+
+};
